e5ec5e919097528459c75caa7c07065d22635c0e
Marcio Lamego <lamegom@me.com>
2020-07-07 00:46:53 -0300

    new fields and sql file

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -2,8 +2,10 @@ package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -12,7 +14,7 @@ import lombok.Data;
 public class Estudante {
 	
 	@Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty(message = "Nome Ã© obrigatÃ³rio")
@@ -22,5 +24,10 @@ public class Estudante {
     private String email;
 
     private String telefone;
+    
+    @NotNull(message = "Matrícula é obrigatorio")
+    private Long matricula;
+    
+    private String curso;
 
 }
